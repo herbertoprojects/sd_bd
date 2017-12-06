b47ef9b13440bffeabafd79b47ed3e13859b08ad
UTILIZADOR <UTILIZADOR@DESKTOP-7CCKKN2>
2017-12-06 13:26:41 +0000

    consola update

--- bd_java_code/src/bd_java_code/Consola.java
+++ bd_java_code/src/bd_java_code/Consola.java
@@ -1,10 +1,30 @@
 package bd_java_code;
 
-public class Consola {
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Consola extends UnicastRemoteObject {
+	
+	private RMI_1 comunicacao;
+	
+	public Consola () throws RemoteException {
+		super();
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+		Consola consola = new Consola();
+		consola.comunicacao = (RMI_1) Naming.lookup("rmi://192.168.0.1/rmi");
+		}
+		catch (RemoteException | MalformedURLException | NotBoundException e){
+			
+		}
+		
 	}
 
 }
